d311204d5e211e38f7fad05910bb939daccc89e6
Aimee Santone <132028651+ARS816@users.noreply.github.com>
2023-08-18 19:21:06 -0700

    Add files via upload

---
+++ LikesAssignment/Part 2/script.js
@@ -0,0 +1,7 @@
+const incrementElement1 = document.querySelector("#increment1");
+const incrementElement2 = document.querySelector("#increment2");
+const incrementElement3 = document.querySelector("#increment3");
+
+function increaseLikes(incrementElement) {
+    incrementElement.innerText++;
+}
